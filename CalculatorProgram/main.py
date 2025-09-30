59dc809bb69c871a2d927be9d01562a11d74530b
Eric-Gitau <gitaueric01@gmail.com>
2025-09-30 14:57:48 +0300

    added asimple calculator program

---
+++ CalculatorProgram/main.py
@@ -0,0 +1,17 @@
+operator = input("Enter an operator (+ - * /)")
+
+num1 = float(input("Enterr the 1st number: "))
+num2 = float(input("Enterr the 2st number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
