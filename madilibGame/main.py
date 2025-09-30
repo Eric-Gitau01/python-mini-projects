7af87971d909908f77595a7808228d34f6f47081
Eric-Gitau <gitaueric01@gmail.com>
2025-09-30 14:35:00 +0300

    Add Mad Libs word game
    Players can create fun stories by filling in blanks with random words.

---
+++ madilibGame/main.py
@@ -0,0 +1,12 @@
+adjective1 = input("Enter and adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+adjective3 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing'")
+
+
+print(f"Today I went to a {adjective1} Zoo")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective1} and {verb1}")
+
+print(f"I was {adjective3}!")
